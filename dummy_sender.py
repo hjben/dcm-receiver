4a5c47b207121263d358e926819ea6031d3a1432
김현중 <hj.ben.kim@gmail.com>
2024-07-29 17:05:03 +0900

    add dummy_sender.py

---
+++ dummy_sender.py
@@ -0,0 +1,39 @@
+"""
+This module has a function to test the dicom receiver.
+
+Functions:
+    - dcm_send: send a dcm file to the target receiver.
+"""
+import os, glob
+import subprocess
+
+from dcm_receiver_config import get_receiver_cfg
+
+receiver_cfg = get_receiver_cfg()
+tmp_receiver_ip = '127.0.0.1' # external IP of receiver
+
+def dcm_send(dcm_path: str):
+    """
+        Send a dcm file to the target receiver and print the result.
+
+        Parameters
+        ----------
+        dcm_path: String
+            dcm file path to send (required)
+
+        Returns
+        -------
+        None
+    """
+    print(f"Try sending DCM file: {dcm_path}")
+    result = subprocess.run(f"python -m pynetdicom storescu {tmp_receiver_ip} {receiver_cfg['PORT']} '{dcm_path}' -v -cx", capture_output=True, text=True, shell=True)
+
+    if (result.stdout + '\n' + result.stderr).find('Success') < 0:
+        print('DCM send failed')
+        print(result.stdout)
+        print(result.stderr)
+    else:
+        print('DCM send successed')
+
+if __name__=="__main__":
+    dcm_send("./send_sample.dcm")
